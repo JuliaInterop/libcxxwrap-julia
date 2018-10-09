c494e7cff1cbd0c59e1fd46ed9eb29a870773065
Bart Janssens <bart@bartjanssens.org>
2018-10-09 21:58:00 +0200

    Support GCC multiversioning

--- .ci/build_tarballs.jl
+++ .ci/build_tarballs.jl
@@ -9,10 +9,17 @@ sources = [
 function getscript(version)
     shortversion = version[1:3]
     return """
+    # Julia_ROOT=\$prefix/julia-$version
+    Julia_ROOT=/usr/local
+
+    # Download julia
+    cd /usr/local
+    curl -L "https://github.com/JuliaPackaging/JuliaBuilder/releases/download/$version/julia-$version-\$target.tar.gz" | tar -zx --strip-components=1 
+
     # Build libcxxwrap
     cd \$WORKSPACE/srcdir/libcxxwrap-julia*
     mkdir build && cd build
-    cmake -DJulia_ROOT=\$prefix/julia-$version -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=/opt/\$target/\$target.toolchain -DCMAKE_CXX_FLAGS="-march=x86-64" -DCMAKE_INSTALL_PREFIX=\${prefix} ..
+    cmake -DJulia_ROOT=\$Julia_ROOT -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=/opt/\$target/\$target.toolchain -DCMAKE_CXX_FLAGS="-march=x86-64" -DCMAKE_INSTALL_PREFIX=\${prefix} ..
     VERBOSE=ON cmake --build . --config Release --target install
     if [[ "\$target" == "x86_64-linux-gnu" ]]; then
         ctest -V
@@ -22,13 +29,18 @@ end
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Linux(:x86_64),
-    MacOS(:x86_64),
-    Windows(:i686),
-    Windows(:x86_64),
-    #Linux(:i686)
-]
+platforms = Platform[]
+_abis(p) = (:gcc7,:gcc8)
+_archs(p) = (:x86_64, :i686)
+_archs(::Type{Linux}) = (:x86_64,)
+for p in (Linux,Windows)
+    for a in _archs(p)
+        for abi in _abis(p)
+            push!(platforms, p(a, compiler_abi=CompilerABI(abi)))
+        end
+    end
+end
+push!(platforms, MacOS(:x86_64))
 
 # The products that we will ensure are always built
 products = prefix -> [
@@ -37,7 +49,7 @@ products = prefix -> [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    "https://github.com/JuliaPackaging/JuliaBuilder/releases/download/1.0.0/build_Julia.v1.0.0.jl"
+ #   "https://github.com/JuliaPackaging/JuliaBuilder/releases/download/1.0.0/build_Julia.v1.0.0.jl"
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
