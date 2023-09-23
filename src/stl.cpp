caa14df01dc73b6920d2c6ce60d8b6240847be55
Abdo Eid <abdoeid.eg@ieee.org>
2023-09-23 19:04:26 +0300

    DequeIterator working now

--- src/stl.cpp
+++ src/stl.cpp
@@ -19,9 +19,8 @@ JLCXX_API void StlWrappers::instantiate(Module& mod)
   m_instance.reset(new StlWrappers(mod));
   m_instance->vector.apply_combination<std::vector, stltypes>(stl::WrapVector());
   m_instance->valarray.apply_combination<std::valarray, stltypes>(stl::WrapValArray());
-  m_instance->deque.apply_combination<std::deque, stltypes>(stl::WrapDeque<stl::DequeIteratorWrapper>());
   m_instance->dequeIterator.apply_combination<stl::DequeIteratorWrapper, stltypes>(stl::WrapIterator());
-  // m_instance->iterator.apply_combination<stl::VectorIteratorWrapper, stltypes>(stl::WrapIterator());
+  m_instance->deque.apply_combination<std::deque, stltypes>(stl::WrapDeque<stl::DequeIteratorWrapper>());
 
   smartptr::apply_smart_combination<std::shared_ptr, stltypes>(mod);
   smartptr::apply_smart_combination<std::weak_ptr, stltypes>(mod);
@@ -97,7 +96,7 @@ JLCXX_MODULE define_cxxwrap_stl_module(jlcxx::Module& stl)
     .method("swap", &std::thread::swap);
 
   stl.method("hardware_concurrency", [] () { return std::thread::hardware_concurrency(); });
-  
+
   jlcxx::add_smart_pointer<std::shared_ptr>(stl, "SharedPtr");
   jlcxx::add_smart_pointer<std::weak_ptr>(stl, "WeakPtr");
   jlcxx::add_smart_pointer<std::unique_ptr>(stl, "UniquePtr");
