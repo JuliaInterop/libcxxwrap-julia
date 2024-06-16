6071314830ea46ceee6368a2fe2aac5cbf1f6af8
Bart Janssens <bart@bartjanssens.org>
2024-06-16 10:40:32 +0200

    Add test for pair comparison
    Also:
    * adds the JLCXX_FORCE_RANGES_OFF compile flag
    * suppresses warnings when compiling without ranges
    * bumps version to v0.13.2

--- include/jlcxx/jlcxx_config.hpp
+++ include/jlcxx/jlcxx_config.hpp
@@ -16,14 +16,14 @@
 
 #define JLCXX_VERSION_MAJOR 0
 #define JLCXX_VERSION_MINOR 13
-#define JLCXX_VERSION_PATCH 0
+#define JLCXX_VERSION_PATCH 2
 
 // From https://stackoverflow.com/questions/5459868/concatenate-int-to-string-using-c-preprocessor
 #define __JLCXX_STR_HELPER(x) #x
 #define __JLCXX_STR(x) __JLCXX_STR_HELPER(x)
 #define JLCXX_VERSION_STRING __JLCXX_STR(JLCXX_VERSION_MAJOR) "." __JLCXX_STR(JLCXX_VERSION_MINOR) "." __JLCXX_STR(JLCXX_VERSION_PATCH)
 
-#if defined(__has_include) && !defined(__FreeBSD__)
+#if defined(__has_include) && !defined(__FreeBSD__) && !defined(JLCXX_FORCE_RANGES_OFF)
 #  if __has_include (<ranges>)
 #    define JLCXX_HAS_RANGES
 #  endif
