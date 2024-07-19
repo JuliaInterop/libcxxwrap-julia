9e875d2b485ee0d891058bed2b7f87e77d57812d
PraneethJain <praneethjain005@gmail.com>
2024-07-19 22:55:49 +0530

    feat: add std::list iterators

--- src/stl_list.cpp
+++ src/stl_list.cpp
@@ -6,6 +6,11 @@ namespace jlcxx
 namespace stl
 {
 
+void apply_list_iterator(TypeWrapper1& list_iterator)
+{
+  list_iterator.apply_combination<stl::ListIteratorWrapper, stltypes>(stl::WrapIterator());
+}
+
 void apply_list(TypeWrapper1& list)
 {
   list.apply_combination<std::list, stltypes>(stl::WrapList());
